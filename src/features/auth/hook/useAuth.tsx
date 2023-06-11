d18b4b6652fb06dc48f2209c6f4853377c3705d4
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2023-06-11 13:19:15 +0400

    Update useAuth.tsx
    Login

--- src/features/auth/hook/useAuth.tsx
+++ src/features/auth/hook/useAuth.tsx
@@ -2,33 +2,31 @@ import { useAppSelector, useAppDispatch } from 'app'
 
 import { authThunks } from 'features/auth/auth.slice'
 
-import {
 
-    redirectPathSelector,
+   
 
-    profileSelector,
+ 
 
-    checkEmailMessageSelector,
+    
 
-    userIdSelector,
+ 
 
-} from 'features/auth/auth.selectors'
 
 import { LoginFieldsType } from 'features/auth/components/Login'
 
 import {
 
-    ForgotPassBodyType,
+   
 
     RegisterBodyType,
 
-    SetNewPassBodyType,
+   
 
-    UpdateProfileBodyType,
+    
 
 } from 'features/auth/auth.api'
 
-import { emailMessage } from 'features/auth/constants'
+
 
 import { useCallback } from 'react'
 
@@ -36,13 +34,12 @@ export const useAuth = () => {
 
     const dispatch = useAppDispatch()
 
-    const profile = useAppSelector(profileSelector)
 
-    const redirectPath = useAppSelector(redirectPathSelector)
+    
 
-    const emailAddress = useAppSelector(checkEmailMessageSelector)
 
-    const authorizedUserId = useAppSelector(userIdSelector)
+
+    
 
     const isProfileDefine = () => Boolean(profile)
 
@@ -84,47 +81,47 @@ export const useAuth = () => {
 
     )
 
-    const forgotPassword = useCallback(
+    
 
-        (data: { email: string }) => {
+       
 
-            const payload: ForgotPassBodyType = {
+            
 
-                email: data.email,
+               
 
-                from: 'App developers ;)',
+              
 
-                message: emailMessage,
+              
 
-            }
 
-            dispatch(authThunks.forgotPassword(payload))
 
-        },
+           
 
-        [dispatch]
+       
 
-    )
+        
 
-    const setNewPassword = useCallback(
+    
 
-        (password: string, token: string) => {
+    
 
-            const payload: SetNewPassBodyType = {
+       
 
-                password,
+            
 
-                resetPasswordToken: token,
+               
 
-            }
+                
 
-            dispatch(authThunks.setNewPassword(payload))
+      
 
-        },
+            
 
-        [dispatch]
+      
 
-    )
+     
+
+
 
     const updateUserName = useCallback(
 
