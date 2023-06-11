d0a4daab26b081638eaa086b4a01949dfae2fbce
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2023-06-11 19:19:52 +0400

    Update useUploadImage.ts
    Avatar

--- src/features/auth/hook/useUploadImage.ts
+++ src/features/auth/hook/useUploadImage.ts
@@ -1,1 +1,44 @@
 
+import { ChangeEvent, useCallback } from 'react'
+
+import { authThunks } from 'features/auth/auth.slice'
+
+import { useAppDispatch } from 'app'
+
+import { convertFileToBase64 } from 'common'
+
+export const useUploadImage = () => {
+
+    const dispatch = useAppDispatch()
+
+    const uploadUserAvatar = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+
+        if (e.target.files && e.target.files.length) {
+
+            const file = e.target.files[0]
+
+            if (file.size < 4000000) {
+
+                convertFileToBase64(file, (avatar: string) => {
+
+                    dispatch(authThunks.updateProfile({ avatar }))
+
+                })
+
+            } else {
+
+                console.error('Error: ', 'the file is too large!')
+
+            }
+
+        }
+
+    }, [])
+
+    return {
+
+        uploadUserAvatar,
+
+    }
+
+}
