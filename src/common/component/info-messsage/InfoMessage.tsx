3a44cf0d9e73070290dab1598e3bdd793eeaabf4
sergeianip20 <94620549+sergeianip20@users.noreply.github.com>
2023-06-12 10:27:03 +0400

    Update InfoMessage.tsx
    Info

--- src/common/component/info-messsage/InfoMessage.tsx
+++ src/common/component/info-messsage/InfoMessage.tsx
@@ -1,1 +1,42 @@
 
+import React, { FC, memo } from 'react'
+
+import Box from '@mui/material/Box'
+
+type InfoMessageType = {
+
+    text: string
+
+    margin?: string
+
+}
+
+export const InfoMessage: FC<InfoMessageType> = memo(({ text, margin }) => {
+
+    return (
+
+        <Box
+
+            component={'span'}
+
+            sx={{
+
+                textAlign: 'center',
+
+                lineHeight: '24px',
+
+                marginTop: margin ? margin : '20px',
+
+                opacity: '0.6',
+
+            }}
+
+        >
+
+            {text}
+
+        </Box>
+
+    )
+
+})
